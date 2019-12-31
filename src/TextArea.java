dbf1815f37a67367a38bfe49cce17a9ac55e5896
Albert Montserrat Royuela <albert@Alberts-MacBook-Pro.local>
2019-12-31 20:52:57 +0100

    Add new button to remove content from the text area

--- src/TextArea.java
+++ src/TextArea.java
@@ -1,8 +1,10 @@
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -27,7 +29,7 @@ public class TextArea {
 class AreaFrame extends JFrame {
 	public AreaFrame() {
 		
-		this.setBounds(500, 300, 500, 250);
+		this.setBounds(400, 300, 400, 250);
 		
 		AreaSheet mySheet = new AreaSheet();
 		
@@ -43,6 +45,8 @@ class AreaFrame extends JFrame {
 class AreaSheet extends JPanel {
 	public AreaSheet() {
 		
+		this.add(new JLabel("The white box below is a JTextArea"));
+		
 		myTextArea = new JTextArea(6, 15);
 		myTextArea.setLineWrap(true);
 		
@@ -50,22 +54,41 @@ class AreaSheet extends JPanel {
 		
 		this.add(scrollSheet);
 		
+		//new Panel to add buttons on it
+		JPanel buttonsPanel = new JPanel();
+		
 		//When clicked, show the written text on the console
-		JButton myButton = new JButton("Display on the console");
+		myButton = new JButton("Display on the console");
 		myButton.addActionListener(new AreaManage());
 		
-		this.add(myButton);
+		//Button to remove content of text area
+		remButton = new JButton("Remove content");
+		remButton.addActionListener(new AreaManage());
+		
+		buttonsPanel.add(myButton);
+		buttonsPanel.add(remButton);
+		
+		this.add(buttonsPanel);
 	}
 	
 	private class AreaManage implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			System.out.println(myTextArea.getText());
+			Object source = e.getSource();
+			
+			if (source == myButton) {	//print on the console
+				System.out.println(myTextArea.getText());
+			}
+			else if (source == remButton) {	//remove content
+				myTextArea.setText(null);
+			}
 		}
 		
 	}
 	
 	
 	private JTextArea myTextArea;
+	private JButton myButton;
+	private JButton remButton;
 }
